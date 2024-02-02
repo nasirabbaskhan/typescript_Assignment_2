1a1792cb4f2daff2fabffc7f511868389351aeff
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-02-02 02:01:21 -0800

    Question 4

---
+++ Q4.js
@@ -0,0 +1,10 @@
+// The code is checking whether a given number is even or odd.
+let a = 25;
+let c = a % 2; // check remander
+if (c == 0) {
+    console.log("even");
+}
+else {
+    console.log("Odd");
+}
+export {};
---
+++ Q4.ts
@@ -0,0 +1,8 @@
+// The code is checking whether a given number is even or odd.
+let a: number = 25;
+let c: number = a % 2; // check remander
+if (c == 0) {
+  console.log("even");
+} else {
+  console.log("Odd");
+}
