4bd561ed0c4b325f895cc1d297d6d03eee404313
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-02-02 02:01:37 -0800

    Question 5

---
+++ Q5.ts
@@ -0,0 +1,9 @@
+// The code is checking if the value of the variable `age` is greater than or equal to 18. If it is, it
+//will print "You are eligible to vote." to the console. Otherwise, it will print "You are not
+//eligible to vote." to the console.
+let age = 19;
+if (age >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote.");
+}
