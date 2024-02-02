951852c14359cd262facae2b3d4323c404f5bc1e
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-02-02 02:00:24 -0800

    Question 2

---
+++ Q2.js
@@ -0,0 +1,7 @@
+//The function calculates the volume of a cube given its length, width, and height.
+function area_cube(length, width, height) {
+    let area_cube = length * width * height;
+    console.log(area_cube);
+}
+area_cube(5, 56, 8);
+export {};
---
+++ Q2.ts
@@ -0,0 +1,8 @@
+//The function calculates the volume of a cube given its length, width, and height.
+
+function area_cube(length: number, width: number, height: number) {
+  let area_cube: number = length * width * height;
+  console.log(area_cube);
+}
+
+area_cube(5, 56, 8);
