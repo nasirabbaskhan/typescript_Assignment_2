65b8db534960dce7a8133756f618cf88e62d04df
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-02-02 02:00:13 -0800

    Question 1

---
+++ Q1.js
@@ -0,0 +1,7 @@
+//The `area_rect` function calculates the area of a rectangle given its length and width.
+function area_rect(length, width) {
+    let area = length * width;
+    return area;
+}
+console.log(area_rect(5, 6));
+export {};
---
+++ Q1.ts
@@ -0,0 +1,8 @@
+//The `area_rect` function calculates the area of a rectangle given its length and width.
+
+function area_rect(length: number, width: number): number {
+  let area: number = length * width;
+  return area;
+}
+
+console.log(area_rect(5, 6));
